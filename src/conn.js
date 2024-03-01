70d08d94d7b2b2e7821b1ec6796f1dd50d35610a
moraleseoscar <oestrada.3@gmail.com>
2024-02-29 18:40:33 -0600

    Node connection to mysql in docker

--- src/conn.js
+++ src/conn.js
@@ -3,13 +3,11 @@ import { config } from 'dotenv'
 config()
 
 const pool = mysql.createPool({
-    // host: process.env.MYSQLDB_HOST,
-    host: 'localhost',
+    host: process.env.MYSQLDB_HOST,
     user: process.env.MYSQLDB_USER,
     database: process.env.MYSQLDB_DB,
     password: process.env.MYSQLDB_PASSWORD,
-    // port: process.env.MYSQLDB_PORT,
-    port: '3307',
+    port: process.env.MYSQLDB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
